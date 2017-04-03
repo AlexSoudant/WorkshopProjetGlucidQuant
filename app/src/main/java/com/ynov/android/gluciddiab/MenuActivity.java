9927487c028337d54615b7587517c22d52271c76
AlexSoudant <asoudant@hotmail.fr>
2017-04-03 16:29:33 +0200

    added spinner categories

--- app/src/main/java/com/ynov/android/gluciddiab/MenuActivity.java
+++ app/src/main/java/com/ynov/android/gluciddiab/MenuActivity.java
@@ -5,7 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.util.ArraySet;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -18,11 +20,16 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.ynov.android.gluciddiab.dataUtils.McDoContract;
 import com.ynov.android.gluciddiab.dataUtils.McDoDbHelper;
 import com.ynov.android.gluciddiab.restoUtils.ImageAdapter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by admin on 01/04/17.
@@ -75,6 +82,11 @@ public class MenuActivity extends AppCompatActivity {
 
     SQLiteDatabase mDb;
     String ItemNames;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -132,6 +144,27 @@ public class MenuActivity extends AppCompatActivity {
 
         cursorProtoRapide.close();
 
+        // cursor categories
+
+        Cursor cursorCat = getItem();
+        cursorCat.moveToFirst();
+
+        //int i = 0;
+        final ArrayList<String> ArrayCat = new ArrayList<String>();
+
+        if (cursorCat.moveToFirst()) {
+            do {
+                ArrayCat.add(cursorCat.getString(cursorCat.getColumnIndex(McDoContract.Entrees.CATEGORIE)));
+            } while (cursorCat.moveToNext());
+        }
+
+        cursorCat.close();
+
+        ArraySet setCat = new ArraySet(ArrayCat);
+        Object[] arraySet = setCat.toArray();
+
+        String[] stringArray = Arrays.copyOf(arraySet, arraySet.length, String[].class);
+
 
         tvPanier = (TextView) findViewById(R.id.textPanier);
         btnValid = (Button) findViewById(R.id.buttonValidPanier);
@@ -148,14 +181,14 @@ public class MenuActivity extends AppCompatActivity {
         restoChoice = extras.getString("EXTRA_RESTO");
 
 
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, liste);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, stringArray);
         dropdown.setAdapter(adapter);
 
         gridview.setAdapter(new ImageAdapter(this));
 
         final ArrayList<String> ArrayPanier = new ArrayList<String>();
         final ArrayList<String> ArrayGluLent = new ArrayList<String>();
-        final ArrayList<String> ArrayGluRapide= new ArrayList<String>();
+        final ArrayList<String> ArrayGluRapide = new ArrayList<String>();
 
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
@@ -194,7 +227,7 @@ public class MenuActivity extends AppCompatActivity {
 
                         //menuListAdapter adapter = new menuListAdapter(MenuActivity.this, ArrayPanier);
 
-                        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(MenuActivity.this,android.R.layout.simple_list_item_1,ArrayPanier);
+                        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(MenuActivity.this, android.R.layout.simple_list_item_1, ArrayPanier);
 
                         lvPanier.setAdapter(adapter);
                         dialog.dismiss();
@@ -229,10 +262,10 @@ public class MenuActivity extends AppCompatActivity {
 
                 Bundle extras = new Bundle();
 
-                extras.putString("EXTRA_MEAL",mealTime);
-                extras.putStringArrayList("EXTRA_MENU",ArrayPanier);
-                extras.putStringArrayList("EXTRA_GLULENT",ArrayGluLent);
-                extras.putStringArrayList("EXTRA_GLURAPIDE",ArrayGluRapide);
+                extras.putString("EXTRA_MEAL", mealTime);
+                extras.putStringArrayList("EXTRA_MENU", ArrayPanier);
+                extras.putStringArrayList("EXTRA_GLULENT", ArrayGluLent);
+                extras.putStringArrayList("EXTRA_GLURAPIDE", ArrayGluRapide);
 
 
                 startPanierActivityIntent.putExtras(extras);
@@ -243,6 +276,9 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
@@ -257,6 +293,42 @@ public class MenuActivity extends AppCompatActivity {
                 null,
                 McDoContract.Entrees.COLUMN_TIMESTAMP
         );
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Menu Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 
     /*private void initiatePopupWindow(View v) {
