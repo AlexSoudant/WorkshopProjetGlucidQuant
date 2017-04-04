25864528b68b13b28beca4cdd70595b5ab94e43f
AlexSoudant <asoudant@hotmail.fr>
2017-04-04 19:13:16 +0200

    added panier link in activity

--- app/src/main/java/com/ynov/android/gluciddiab/PanierActivity.java
+++ app/src/main/java/com/ynov/android/gluciddiab/PanierActivity.java
@@ -6,9 +6,10 @@ package com.ynov.android.gluciddiab;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -31,8 +32,6 @@ import java.util.List;
 
 
 public class PanierActivity extends AppCompatActivity {
-
-    private Toolbar toolbar;
     ListView mListView;
     private ProgressBar mProgressBar1;
     private ProgressBar mProgressBar2;
@@ -42,7 +41,8 @@ public class PanierActivity extends AppCompatActivity {
     private TextView textMaxTGL;
     private RadioButton mRadio;
     private List<String> protocoles;
-    String[] prenoms = new String[]{
+    private CustomListView adapter;
+    /*String[] prenoms = new String[]{
             "Big Mac Maxi", "frite Maxi", "coca-cola", "sunday"
     };
     int[] gr = new int[]{
@@ -50,28 +50,32 @@ public class PanierActivity extends AppCompatActivity {
     };
     int[] gl = new int[]{
             6,7,5,6
-    };
+    };*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.panier);
 
-
-        toolbar = (Toolbar) findViewById(R.id.app_bar);
-        setSupportActionBar(toolbar);
-
         Intent intentThatStartedThisActivity = getIntent();
 
         Bundle extras = intentThatStartedThisActivity.getExtras();
         final String mealTime = extras.getString("EXTRA_MEAL");
-        final ArrayList<String> menuChoice = extras.getStringArrayList("EXTRA_MENU");
-        final ArrayList<String> GluLent = extras.getStringArrayList("EXTRA_GLULENT");
-        final ArrayList<String> GluRapide = extras.getStringArrayList("EXTRA_GLURAPIDE");
+        final ArrayList<String> prenoms = extras.getStringArrayList("EXTRA_MENU");
+        final ArrayList<String> gl = extras.getStringArrayList("EXTRA_GLULENT");
+        final ArrayList<String> gr = extras.getStringArrayList("EXTRA_GLURAPIDE");
 
         //recuperation des protocols
         ProtocoleGlucidesDbHelper db = new ProtocoleGlucidesDbHelper(this);
-        protocoles= db.getProtocole(1);
+        if(mealTime.equals("matin")){
+            protocoles= db.getProtocole(0);
+        }else if(mealTime.equals("midi")){
+            protocoles= db.getProtocole(1);
+        }else if(mealTime.equals("gouter")){
+            protocoles= db.getProtocole(2);
+        }else if(mealTime.equals("soir")){
+            protocoles= db.getProtocole(3);
+        }
 
         mProgressBar1 = (ProgressBar) findViewById(R.id.pGL);
         mProgressBar1.setMax(Integer.parseInt(protocoles.get(1)));
@@ -81,7 +85,7 @@ public class PanierActivity extends AppCompatActivity {
         textTGR = (TextView) findViewById(R.id.textTGR);
         textMaxTGL = (TextView) findViewById(R.id.textMaxTGL);
         textMaxTGR = (TextView) findViewById(R.id.textMaxTGR);
-        CustomListView adapter = new
+        adapter = new
                 CustomListView(PanierActivity.this,prenoms,gr,gl,mProgressBar1,mProgressBar2,textTGL,textTGR);
         mListView = (ListView) findViewById(R.id.listView);
         mListView.setAdapter(adapter);
@@ -101,13 +105,13 @@ public class PanierActivity extends AppCompatActivity {
 
             }
         });
-
-
     }
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
 
+
+        double[] totalGlu=adapter.getGluActuelle();
         // Check which radio button was clicked
         switch(view.getId()) {
             case R.id.radio_1:
@@ -116,6 +120,8 @@ public class PanierActivity extends AppCompatActivity {
                 mProgressBar2.setMax((int)(Integer.parseInt(protocoles.get(2))*1));
                 textMaxTGL.setText("Max "+Integer.parseInt(protocoles.get(1))*1+"g");
                 textMaxTGR.setText("Max "+Integer.parseInt(protocoles.get(2))*1+"g");
+                mProgressBar1.setProgress((int)totalGlu[0]);
+                mProgressBar2.setProgress((int)totalGlu[1]);
                 break;
             case R.id.radio_2:
                 if (checked)
@@ -123,6 +129,8 @@ public class PanierActivity extends AppCompatActivity {
                 mProgressBar2.setMax((int)(Integer.parseInt(protocoles.get(2))*1.5));
                 textMaxTGL.setText("Max "+Integer.parseInt(protocoles.get(1))*1.5+"g");
                 textMaxTGR.setText("Max "+Integer.parseInt(protocoles.get(2))*1.5+"g");
+                mProgressBar1.setProgress((int)totalGlu[0]);
+                mProgressBar2.setProgress((int)totalGlu[1]);
                 break;
             case R.id.radio_3:
                 if (checked)
@@ -130,6 +138,8 @@ public class PanierActivity extends AppCompatActivity {
                 mProgressBar2.setMax((int)(Integer.parseInt(protocoles.get(2))*2));
                 textMaxTGL.setText("Max "+Integer.parseInt(protocoles.get(1))*2+"g");
                 textMaxTGR.setText("Max "+Integer.parseInt(protocoles.get(2))*2+"g");
+                mProgressBar1.setProgress((int)totalGlu[0]);
+                mProgressBar2.setProgress((int)totalGlu[1]);
                 break;
             case R.id.radio_4:
                 if (checked)
@@ -137,12 +147,27 @@ public class PanierActivity extends AppCompatActivity {
                 mProgressBar2.setMax((int)(Integer.parseInt(protocoles.get(2))*3));
                 textMaxTGL.setText("Max "+Integer.parseInt(protocoles.get(1))*3+"g");
                 textMaxTGR.setText("Max "+Integer.parseInt(protocoles.get(2))*3+"g");
+                mProgressBar1.setProgress((int)totalGlu[0]);
+                mProgressBar2.setProgress((int)totalGlu[1]);
                 break;
         }
+
+        //couleur des progressBar
+        int p1v=mProgressBar1.getProgress();
+        int p1m=mProgressBar1.getMax();
+        if(p1v ==p1m){
+            mProgressBar1.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+        }else{
+            mProgressBar1.getProgressDrawable().setColorFilter(Color.parseColor("#00A6FF"), PorterDuff.Mode.SRC_IN);
+        }
+        int p2v=mProgressBar2.getProgress();
+        int p2m=mProgressBar2.getMax();
+        if(p2v == p2m){
+            mProgressBar2.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+        }else{
+            mProgressBar2.getProgressDrawable().setColorFilter(Color.parseColor("#00A6FF"), PorterDuff.Mode.SRC_IN);
+        }
     }
-
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/ynov/android/gluciddiab/panierUtils/CustomListView.java
+++ app/src/main/java/com/ynov/android/gluciddiab/panierUtils/CustomListView.java
@@ -5,31 +5,30 @@ package com.ynov.android.gluciddiab.panierUtils;
  */
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.ynov.android.gluciddiab.R;
-//import com.ynov.android.gluciddiab.calculUtils.Panier;
 
-import java.util.Arrays;
-import java.util.stream.*;
+import java.util.ArrayList;
+
+//import com.ynov.android.gluciddiab.calculUtils.Panier;
 
 public class CustomListView extends ArrayAdapter<String>{
 
     private final Activity context;
-    private final String[] textName;
-    private final int[] textGR;
-    private final int[] textGL;
+    private final ArrayList<String>  textName;
+    private final ArrayList<String> textGR;
+    private final ArrayList<String> textGL;
     private final double[] textGRActuel;
     private final double[] textGLActuel;
     private TextView textViewGR;
@@ -39,7 +38,7 @@ public class CustomListView extends ArrayAdapter<String>{
     private Switch mSwitch;
 
     public CustomListView(Activity context,
-                          String[] textName, int[] textGR,int[] textGL,
+                          ArrayList<String>  textName, ArrayList<String> textGR,ArrayList<String> textGL,
                           ProgressBar mProgressBar1,ProgressBar mProgressBar2,TextView textViewGL,TextView textViewGR) {
         super(context, R.layout.row_list, textName);
         this.context = context;
@@ -48,7 +47,7 @@ public class CustomListView extends ArrayAdapter<String>{
         this.textGL = textGL;
         this.mProgressBar1=mProgressBar1;
         this.mProgressBar2=mProgressBar2;
-        int num= textGL.length;
+        int num= textGL.size();
         this.textGLActuel= new double[num];
         this.textGRActuel=new double[num];
         this.textViewGL = textViewGL;
@@ -64,11 +63,11 @@ public class CustomListView extends ArrayAdapter<String>{
         TextView txtTitle2 = (TextView) rowView.findViewById(R.id.textViewGR);
         final RadioGroup radio = (RadioGroup) rowView.findViewById(R.id.radioP);
         mSwitch= (Switch)rowView.findViewById(R.id.toggle1);
-        txtTitle.setText(textName[position]);
-        txtTitle1.setText("glucide rapide: "+textGR[position]+"g");
-        txtTitle2.setText("glucide lent: "+ textGL[position]+"g");
-        textGLActuel[position]=(textGL[position]*(1));
-        textGRActuel[position]=(textGR[position]*(1));
+        txtTitle.setText(textName.get(position));
+        txtTitle1.setText("glucide rapide: "+textGR.get(position)+"g");
+        txtTitle2.setText("glucide lent: "+ textGL.get(position)+"g");
+        textGLActuel[position]=(Integer.parseInt(textGL.get(position))*(1));
+        textGRActuel[position]=(Integer.parseInt(textGR.get(position))*(1));
         int i = 0;
         double sum=0;
         while(i < textGLActuel.length) {
@@ -112,26 +111,42 @@ public class CustomListView extends ArrayAdapter<String>{
 
                     textViewGR.setText("Total de glucide rapide:"+sum+"g");
                     mProgressBar2.setProgress((int)(sum));
+
+                    //change couleur si max
+                    int p1v=mProgressBar1.getProgress();
+                    int p1m=mProgressBar1.getMax();
+                    if(p1v ==p1m){
+                        mProgressBar1.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+                    }else{
+                        mProgressBar1.getProgressDrawable().setColorFilter(Color.parseColor("#00A6FF"), PorterDuff.Mode.SRC_IN);
+                    }
+                    int p2v=mProgressBar2.getProgress();
+                    int p2m=mProgressBar2.getMax();
+                    if(p2v == p2m){
+                        mProgressBar2.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+                    }else{
+                        mProgressBar2.getProgressDrawable().setColorFilter(Color.parseColor("#00A6FF"), PorterDuff.Mode.SRC_IN);
+                    }
                 }else{
                     for (int i = 0; i < radio.getChildCount(); i++) {
                         radio.getChildAt(i).setEnabled(true);
                     }
                     switch (radio.getCheckedRadioButtonId()){
                         case R.id.radioP_1:
-                            textGLActuel[position]=(textGL[position]*(0.25));
-                            textGRActuel[position]=(textGR[position]*(0.25));
+                            textGLActuel[position]=(Integer.parseInt(textGL.get(position))*(0.25));
+                            textGRActuel[position]=(Integer.parseInt(textGR.get(position))*(0.25));
                             break;
                         case R.id.radioP_2:
-                            textGLActuel[position]=(textGL[position]*(0.5));
-                            textGRActuel[position]=(textGR[position]*(0.5));
+                            textGLActuel[position]=(Integer.parseInt(textGL.get(position))*(0.5));
+                            textGRActuel[position]=(Integer.parseInt(textGR.get(position))*(0.5));
                             break;
                         case R.id.radioP_3:
-                            textGLActuel[position]=(textGL[position]*(0.75));
-                            textGRActuel[position]=(textGR[position]*(0.75));
+                            textGLActuel[position]=(Integer.parseInt(textGL.get(position))*(0.75));
+                            textGRActuel[position]=(Integer.parseInt(textGR.get(position))*(0.75));
                             break;
                         case R.id.radioP_4:
-                            textGLActuel[position]=(textGL[position]*(1));
-                            textGRActuel[position]=(textGR[position]*(1));
+                            textGLActuel[position]=(Integer.parseInt(textGL.get(position))*(1));
+                            textGRActuel[position]=(Integer.parseInt(textGR.get(position))*(1));
                             break;
                         default:
                             break;
@@ -153,6 +168,21 @@ public class CustomListView extends ArrayAdapter<String>{
 
                     textViewGR.setText("Total de glucide rapide:"+sum+"g");
                     mProgressBar2.setProgress((int)(sum));
+                    //change couleur si max
+                    int p1v=mProgressBar1.getProgress();
+                    int p1m=mProgressBar1.getMax();
+                    if(p1v ==p1m){
+                        mProgressBar1.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+                    }else{
+                        mProgressBar1.getProgressDrawable().setColorFilter(Color.parseColor("#00A6FF"), PorterDuff.Mode.SRC_IN);
+                    }
+                    int p2v=mProgressBar2.getProgress();
+                    int p2m=mProgressBar2.getMax();
+                    if(p2v == p2m){
+                        mProgressBar2.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+                    }else{
+                        mProgressBar2.getProgressDrawable().setColorFilter(Color.parseColor("#00A6FF"), PorterDuff.Mode.SRC_IN);
+                    }
                 }
             }
         });
@@ -164,26 +194,26 @@ public class CustomListView extends ArrayAdapter<String>{
                     case R.id.radioP_1:
                         // mProgressBar1.setProgress((int)(mProgressBar1.getProgress()-textGLActuel[position]+textGL[position]*(0.25)));
                         //mProgressBar2.setProgress((int)(mProgressBar2.getProgress()-textGRActuel[position]+textGR[position]*(0.25)));
-                        textGLActuel[position]=(textGL[position]*(0.25));
-                        textGRActuel[position]=(textGR[position]*(0.25));
+                        textGLActuel[position]=(Integer.parseInt(textGL.get(position))*(0.25));
+                        textGRActuel[position]=(Integer.parseInt(textGR.get(position))*(0.25));
                         break;
                     case R.id.radioP_2:
                         //mProgressBar1.setProgress((int)(mProgressBar1.getProgress()-textGLActuel[position]+textGL[position]*(0.5)));
                         //mProgressBar2.setProgress((int)(mProgressBar2.getProgress()-textGRActuel[position]+textGR[position]*(0.5)));
-                        textGLActuel[position]=(textGL[position]*(0.5));
-                        textGRActuel[position]=(textGR[position]*(0.5));
+                        textGLActuel[position]=(Integer.parseInt(textGL.get(position))*(0.5));
+                        textGRActuel[position]=(Integer.parseInt(textGR.get(position))*(0.5));
                         break;
                     case R.id.radioP_3:
                         //mProgressBar1.setProgress((int)(mProgressBar1.getProgress()-textGLActuel[position]+textGL[position]*(0.75)));
                         // mProgressBar2.setProgress((int)(mProgressBar2.getProgress()-textGRActuel[position]+textGR[position]*(0.75)));
-                        textGLActuel[position]=(textGL[position]*(0.75));
-                        textGRActuel[position]=(textGR[position]*(0.75));
+                        textGLActuel[position]=(Integer.parseInt(textGL.get(position))*(0.75));
+                        textGRActuel[position]=(Integer.parseInt(textGR.get(position))*(0.75));
                         break;
                     case R.id.radioP_4:
                         //mProgressBar1.setProgress((int)(mProgressBar1.getProgress()-textGLActuel[position]+textGL[position]*(1)));
                         //mProgressBar2.setProgress((int)(mProgressBar2.getProgress()-textGRActuel[position]+textGR[position]*(1)));
-                        textGLActuel[position]=(textGL[position]*(1));
-                        textGRActuel[position]=(textGR[position]*(1));
+                        textGLActuel[position]=(Integer.parseInt(textGL.get(position))*(1));
+                        textGRActuel[position]=(Integer.parseInt(textGR.get(position))*(1));
                         break;
                     default:
                         break;
@@ -205,9 +235,57 @@ public class CustomListView extends ArrayAdapter<String>{
 
                 textViewGR.setText("Total de glucide rapide:"+sum+"g");
                 mProgressBar2.setProgress((int)(sum));
+                //change couleur si max
+                int p1v=mProgressBar1.getProgress();
+                int p1m=mProgressBar1.getMax();
+                if(p1v ==p1m){
+                    mProgressBar1.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+                }else{
+                    mProgressBar1.getProgressDrawable().setColorFilter(Color.parseColor("#00A6FF"), PorterDuff.Mode.SRC_IN);
+                }
+                int p2v=mProgressBar2.getProgress();
+                int p2m=mProgressBar2.getMax();
+                if(p2v == p2m){
+                    mProgressBar2.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+                }else{
+                    mProgressBar2.getProgressDrawable().setColorFilter(Color.parseColor("#00A6FF"), PorterDuff.Mode.SRC_IN);
+                }
+
             }
         });
 
+        //initialisation couleur des progressBar
+        int p1v=mProgressBar1.getProgress();
+        int p1m=mProgressBar1.getMax();
+        if(p1v ==p1m){
+            mProgressBar1.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+        }else{
+            mProgressBar1.getProgressDrawable().setColorFilter(Color.parseColor("#00A6FF"), PorterDuff.Mode.SRC_IN);
+        }
+        int p2v=mProgressBar2.getProgress();
+        int p2m=mProgressBar2.getMax();
+        if(p2v == p2m){
+            mProgressBar2.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+        }else{
+            mProgressBar2.getProgressDrawable().setColorFilter(Color.parseColor("#00A6FF"), PorterDuff.Mode.SRC_IN);
+        }
         return rowView;
+    }
+
+    public double[] getGluActuelle(){
+        int i = 0;
+        double sum=0;
+        while(i < textGLActuel.length) {
+            sum += textGLActuel[i];
+            i++;
+        }
+        i = 0;
+        double sum2=0;
+        while(i < textGRActuel.length) {
+            sum2 += textGRActuel[i];
+            i++;
+        }
+        double[] res= new double[]{sum,sum2};
+        return res;
     }
 }
